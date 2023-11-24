dea0d13a84acb773508836f8c73ffbb819cd0349
Promzmunac <Promzmunac>
2023-11-24 20:55:16 +0100

    citiflo2 commit

--- app/src/main/java/com/munachi/citiflo/navfragments/HomeFragment.kt
+++ app/src/main/java/com/munachi/citiflo/navfragments/HomeFragment.kt
@@ -4,6 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Toast
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
@@ -38,8 +41,35 @@ class HomeFragment : Fragment(), LikedOnClickInterface {
         databaseReference = FirebaseDatabase.getInstance().getReference("products")
         auth = FirebaseAuth.getInstance()
 
+        Spinner()
+
     }
 
+    fun Spinner(){
+
+        val nigerianStates = resources.getStringArray(R.array.Nigeria)
+        val spinner = binding.spinnerDropDown
+        val adapter = ArrayAdapter(requireContext(),
+            android.R.layout.simple_spinner_item, nigerianStates)
+        spinner.adapter = adapter
+
+
+        spinner.onItemSelectedListener = object :
+
+            AdapterView.OnItemSelectedListener {
+
+            override fun onItemSelected(parent: AdapterView<*>,view: View, position: Int, id: Long) {
+
+                val msg = getString(R.string.Nigeria_Displayed) + " " + "" + nigerianStates[position]
+
+                Toast.makeText(requireContext(), msg, Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>) {
+                // write code to perform some action
+            }
+        }
+    }
     override fun onClickLike(item: LikeModel) {
         TODO("Not yet implemented")
     }
